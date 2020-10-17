dcef192b162361cba5d405e5c24166e1b36e666c
Josh Noel <obsidize@gmail.com>
2020-10-17 12:13:25 -0500

    more bulldozer updates

---
+++ src/config-context.ts
@@ -0,0 +1,46 @@
+import { toString, toNumber, toLower, some, forEach } from 'lodash';
+
+export enum DotEnvConfigVariableType {
+	string = 'string',
+	boolean = 'boolean',
+	number = 'number'
+}
+
+export interface DotEnvConfigVariableEntry {
+	name: string;
+	type: DotEnvConfigVariableType;
+	defaultValue: string | boolean | number;
+}
+
+export interface DotEnvConfigContext {
+	entries: DotEnvConfigVariableEntry[];
+}
+
+export const toBoolean = (value: any): boolean => {
+	const normalized = toLower(toString(value));
+	return some(['true', '1'], v => v === normalized);
+};
+
+export const coerceDotEnvConfigVariableType = (type: DotEnvConfigVariableType, value: any): any => {
+	switch (type) {
+		case DotEnvConfigVariableType.boolean:
+			return toBoolean(value);
+		case DotEnvConfigVariableType.number:
+			return toNumber(value);
+		case DotEnvConfigVariableType.string:
+		default:
+			return toString(value);
+	}
+};
+
+export const parseConfigContextFromJson = (rawJsonConfig: any): DotEnvConfigContext => {
+
+	const result: DotEnvConfigContext = { entries: [] };
+	const { entries } = (rawJsonConfig || {});
+
+	forEach(entries, ({ type, defaultValue }, name) => {
+		result.entries.push({ name, type, defaultValue });
+	});
+
+	return result;
+};
